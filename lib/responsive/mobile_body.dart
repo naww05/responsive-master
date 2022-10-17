f1bdfc23a7ddecd933d31012c3d8449cdf9bf435
naww <krisnaww87@gmail.com>
2022-10-17 23:04:49 +0800

    first commit

---
+++ lib/responsive/mobile_body.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class MyMobileBody extends StatelessWidget {
+  const MyMobileBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('M O B I L E')),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            // youtube video
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: AspectRatio(
+                aspectRatio: 16 / 9,
+                child: Container(
+                  color: Colors.yellow,
+                ),
+              ),
+            ),
+
+            // comment section & recommended videos
+            Expanded(
+              child: ListView.builder(
+                itemCount: 8,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      color: Colors.red,
+                      height: 120,
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
